433bc3c49747bfd9647744a1ce565b392f792d94
Ranjith <velusamyranjith@gmail.com>
2026-04-10 12:21:59 +0530

    ready to publish

---
+++ example/android/app/src/main/kotlin/com/ranjith/gps_camera/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ranjith.geo_tag_camera
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/geo_tag_camera.dart
@@ -0,0 +1,7 @@
+export 'src/camera_page.dart';
+export 'src/camera_settings.dart';
+export 'src/geo_image_watermark.dart';
+export 'src/grid_overlay.dart';
+export 'src/permission_service.dart';
+export 'src/preview_page.dart';
+export 'src/sensor_overlay.dart';
